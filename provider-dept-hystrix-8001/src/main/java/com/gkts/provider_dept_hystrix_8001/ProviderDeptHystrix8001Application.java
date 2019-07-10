d501b364e7c1ecdbd1df1642db71475a3030c8a7
lym <luoym@gktsingchip.com>
2019-07-10 18:10:16 +0800

    实现服务监控hystrixDashboard

--- provider-dept-hystrix-8001/src/main/java/com/gkts/provider_dept_hystrix_8001/ProviderDeptHystrix8001Application.java
+++ provider-dept-hystrix-8001/src/main/java/com/gkts/provider_dept_hystrix_8001/ProviderDeptHystrix8001Application.java
@@ -1,14 +1,30 @@
 package com.gkts.provider_dept_hystrix_8001;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker//对hystrixR熔断机制的支持
+@EnableHystrix
+@EnableCircuitBreaker//对hystrix熔断机制的支持
 public class ProviderDeptHystrix8001Application {
+    /*2.x以上版本需要实现服务被监控需要添加的内容
+    * ("/actuator/hystrix.stream");这个要和你的hystrix-turbine中的默认访问链接一致，不然还是无法访问的*/
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderDeptHystrix8001Application.class, args);
